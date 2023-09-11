360ccdd36888a83a5458324dc7070010b9b187bb
LAKSHMI A <108331858+ALPV488@users.noreply.github.com>
2023-09-11 21:39:58 +0530

    Add files via upload

---
+++ SpringCoreUsingMaven/src/main/java/com/SpringCoreUsingMaven/Employee.java
@@ -0,0 +1,66 @@
+package com.SpringCoreUsingMaven;
+
+public class Employee {
+	
+	private int empId;
+	
+	private String empName;
+	
+	private int empSal;
+	
+	private String empAdd;
+	
+	
+	
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getEmpName() {
+		return empName;
+	}
+
+	public void setEmpName(String empName) {
+		this.empName = empName;
+	}
+
+	public int getEmpSal() {
+		return empSal;
+	}
+
+	public void setEmpSal(int empSal) {
+		this.empSal = empSal;
+	}
+
+	public String getEmpAdd() {
+		return empAdd;
+	}
+
+	public void setEmpAdd(String empAdd) {
+		this.empAdd = empAdd;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", empName=" + empName + ", empSal=" + empSal + ", empAdd=" + empAdd + "]";
+	}
+	
+	public Employee() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employee(int empId, String empName, int empSal, String empAdd) {
+		super();
+		this.empId = empId;
+		this.empName = empName;
+		this.empSal = empSal;
+		this.empAdd = empAdd;
+	}
+	
+
+}
