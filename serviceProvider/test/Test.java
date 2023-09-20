ff49ad0e81e4d741d8281e73d979c337d000d6a5
LAKSHMI A <108331858+ALPV488@users.noreply.github.com>
2023-09-20 18:00:59 +0530

    Add files via upload

---
+++ serviceProvider/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+import person.Person;
+
+public interface Test {
+
+	public boolean addPerson(Person p);
+	
+	public boolean deletePerson(int id);
+	
+	public Person getPerson(int id);
+	
+	public Person[] getAllPersons();
+}
